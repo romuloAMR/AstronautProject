e5a3918c42c84ed5e5f6341328aa27158e05b039
romuloAMR <amr.rmulo@gmail.com>
2024-04-26 08:29:29 -0300

    main finished

--- main.cpp
+++ main.cpp
@@ -2,13 +2,13 @@
 #include "Headers/fakedb.h"
 
 short homeMenu(FakeDB* &db){
-    std::cout << "------------- Menu -------------\n"
-              << "| 1- Add Astronaut             |\n"
-              << "| 2- Add Spacecraft            |\n"
-              << "| 3- Spacecraft option         |\n"
-              << "| 4- List all dead astronauts  |\n"
-              << "| 5- End                       |\n"
-              << "--------------------------------\n"
+    std::cout << "------------ Menu: ------------\n"
+              << "| 1- Add Astronaut            |\n"
+              << "| 2- Add Spacecraft           |\n"
+              << "| 3- Spacecraft option        |\n"
+              << "| 4- List all dead astronauts |\n"
+              << "| 5- End                      |\n"
+              << "-------------------------------\n"
               <<std::endl;
     std::cout << "Choise a option: ";
     
@@ -18,25 +18,20 @@ short homeMenu(FakeDB* &db){
     case 1: db->registerAstronaut(); return 0;
     case 2: db->registerSpacecraft(); return 0;
     case 3: return 1;
-    case 4: /*code*/ return 0;
+    case 4: db->listAllDeadAstronauts(); return 0;
     case 5: return -1;
     default: std::cout << "Invalid option!" << std::endl; return 0;
     }
 }
 
 short spacecraftMenu(FakeDB* &db){
-    std::cout << "------ Spacecraft Menu ------|\n"
-              << "| 1- Add Astronaut           |\n"
-              << "| 2- Remove Astronaut        |\n"
-              << "| 3- Launch Spacecraft       |\n"
-              << "| 4- Destroy Spacecraft      |\n"
-              << "| 5- Finalize Launch         |\n"
-              << "| 6- Reuse Spacecraft        |\n"
-              << "| 7- List planned Launches   |\n"
-              << "| 8- List Launches           |\n"
-              << "| 9- List finalized Launches |\n"
-              << "| 10- Exit                   |\n"
-              << "------------------------------\n"
+    std::cout << "-------------------- Spacecraft Menu: ---------------------\n"
+              << "| 1- Add Astronaut           | 6- Reuse Spacecraft        |\n"
+              << "| 2- Remove Astronaut        | 7- List planned Launches   |\n"
+              << "| 3- Launch Spacecraft       | 8- List Launches           |\n"
+              << "| 4- Destroy Spacecraft      | 9- List finalized Launches |\n"
+              << "| 5- Finalize Launch         | 10- Exit                   |\n"
+              << "-----------------------------------------------------------\n"
               << std::endl;
     std::cout << "Choise a option: ";
 
@@ -45,13 +40,13 @@ short spacecraftMenu(FakeDB* &db){
     switch (option){
         case 1: db->addAstronautInSpacecraft(); return 1;
         case 2: db->removeAstronautInSpacecraft(); return 1;
-        case 3: /*code*/ return 1;
-        case 4: /*code*/ return 1;
-        case 5: /*code*/ return 1;
-        case 6: /*code*/ return 1;
-        case 7: /*code*/ return 1;
-        case 8: /*code*/ return 1;
-        case 9: /*code*/ return 1;
+        case 3: db->launchSpacecraft(); return 1;
+        case 4: db->destroySpacecraft(); return 1;
+        case 5: db->finalizeLaunch(); return 1;
+        case 6: db->reuseSpacecraft(); return 1;
+        case 7: db->listPlannedLaunches(); return 1;
+        case 8: db->listLaunches(); return 1;
+        case 9: db->listFinalizedLaunches(); return 1;
         case 10: return 0;
         default: std::cout << "Invalid option!" << std::endl; return 1;
     }
